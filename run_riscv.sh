16edc548376c77a67aacf0087dba507425feb964
yjl9903 <yjl9903@onekuma.cn>
2024-10-27 19:44:38 +0800

    chore: run riscv

---
+++ run_riscv.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+filepath=$1
+filename=$(basename "$filepath")
+filename="${filename%.*}"
+
+moon run src/bin/main.mbt -- "$filepath" -o "temp/${filename}.s" || exit $?
+
+zig build-exe -target riscv64-linux "-femit-bin=temp/${filename}" \
+  "temp/${filename}.s" /runtime/riscv_rt/zig-out/lib/libmincaml.a \
+  -O Debug -fno-strip -mcpu=baseline_rv64 || exit $?
+
+rvlinux -n "temp/${filename}"
