28b7e396f0093c4f89b5a6db0ec4661a7f17488e
yjl9903 <yjl9903@onekuma.cn>
2024-10-29 18:04:13 +0800

    fix(riscv): run scripts

--- run_riscv.sh
+++ run_riscv.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+mkdir -p temp
+if [ ! -e "riscv_rt/zig-out/lib/libmincaml.a" ]; then
+  ./build_riscvrt.sh
+fi
+
 filepath=$1
 filename=$(basename "$filepath")
 filename="${filename%.*}"
@@ -7,7 +12,11 @@ filename="${filename%.*}"
 moon run src/bin/main.mbt -- "$filepath" -o "temp/${filename}.s" || exit $?
 
 zig build-exe -target riscv64-linux "-femit-bin=temp/${filename}" \
-  "temp/${filename}.s" /runtime/riscv_rt/zig-out/lib/libmincaml.a \
+  "temp/${filename}.s" "riscv_rt/zig-out/lib/libmincaml.a" \
   -O Debug -fno-strip -mcpu=baseline_rv64 || exit $?
 
-./rvlinux -n "temp/${filename}"
+if [ "$2" == "-o" ]; then
+  ./rvlinux -n "temp/${filename}" > "temp/${filename}.out"
+else
+  ./rvlinux -n "temp/${filename}"
+fi
--- run_wasm.sh
+++ run_wasm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p temp
+
 filepath=$1
 filename=$(basename "$filepath")
 filename="${filename%.*}"
@@ -7,8 +9,8 @@ filename="${filename%.*}"
 moon run src/bin --debug -- --wasm "$filepath" -o "temp/${filename}.wat" || exit $?
 wasm-tools parse "temp/${filename}.wat" -o "temp/${filename}.wasm" || exit $?
 
-if [ -z "$2" ]; then
-  echo '' | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm"
-else
+if [ "$2" == "-o" ]; then
   echo '' | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm" > "temp/${filename}.out"
+else
+  echo '' | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm"
 fi
