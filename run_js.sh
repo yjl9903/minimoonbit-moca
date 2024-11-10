ddcf9c23eccd0dd4a188bcdd54a684a3e1b827aa
yjl9903 <yjl9903@onekuma.cn>
2024-11-10 23:07:22 +0800

    chore: update scripts

--- run_js.sh
+++ run_js.sh
@@ -6,10 +6,20 @@ filepath=$1
 filename=$(basename "$filepath")
 filename="${filename%.*}"
 
+# Input file: -i in.txt
+shift
+if [ "$1" == "-i" ]; then
+  input="$2"
+  shift
+  shift
+else
+  input="/dev/null"
+fi
+
 moon run src/bin/main.mbt --debug -- --js "$filepath" -o "temp/${filename}.mjs" || exit $?
 
-if [ "$2" == "-o" ]; then
-  echo '' | node ./js_rt/runtime.mjs "temp/${filename}.mjs" > "temp/${filename}.out"
+if [ "$1" == "-o" ]; then
+  cat "$input" | node ./js_rt/runtime.mjs "temp/${filename}.mjs" > "temp/${filename}.out"
 else
-  echo '' | node ./js_rt/runtime.mjs "temp/${filename}.mjs"
+  cat "$input" | node ./js_rt/runtime.mjs "temp/${filename}.mjs"
 fi
--- run_wasm.sh
+++ run_wasm.sh
@@ -6,11 +6,21 @@ filepath=$1
 filename=$(basename "$filepath")
 filename="${filename%.*}"
 
+# Input file: -i in.txt
+shift
+if [ "$1" == "-i" ]; then
+  input="$2"
+  shift
+  shift
+else
+  input="/dev/null"
+fi
+
 moon run src/bin --debug -- --wasm "$filepath" -o "temp/${filename}.wat" || exit $?
 wasm-tools parse "temp/${filename}.wat" -o "temp/${filename}.wasm" || exit $?
 
-if [ "$2" == "-o" ]; then
-  echo '' | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm" > "temp/${filename}.out"
+if [ "$1" == "-o" ]; then
+  cat "$input" | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm" > "temp/${filename}.out"
 else
-  echo '' | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm"
+  cat "$input" | node ./wasm_rt/runtime.mjs "temp/${filename}.wasm"
 fi
