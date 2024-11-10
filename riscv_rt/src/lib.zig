e446fc321c68041a1f6dc3054b51af9d5ce97f04
XLor <yjl9903@vip.qq.com>
2024-11-11 03:24:17 +0800

    fix: print double format

--- riscv_rt/src/lib.zig
+++ riscv_rt/src/lib.zig
@@ -45,7 +45,7 @@ export fn minimbt_print_int(i: i32) void {
 }
 
 export fn minimbt_print_double(i: f64) void {
-    _ = stdout.writer().print("{:.2}", .{i}) catch 0;
+    _ = stdout.writer().print("{d}", .{i}) catch 0;
 }
 
 export fn minimbt_print_endline() void {
